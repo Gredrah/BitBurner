bc7ef7551c346cb9c8012212af70779abc88bc15
Gredra2656 <99298563+Gredra2656@users.noreply.github.com>
2026-02-10 22:48:12 -0800

    Uploading templates

---
+++ scripts/hacking/first_hack_pattern.js
@@ -0,0 +1,18 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  const target = ns.args[0];
+  const moneyThresh = ns.getServerMaxMoney * .75;
+  const secThresh = ns.getServerMinSecurityLevel(target) + 5;
+
+  while (true) {
+    if (ns.getServerSecurityLevel(target) > secThresh) {
+      await ns.weaken(target);
+    } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+      await ns.grow(target);
+    } else {
+      await ns.hack(target);
+    }
+  }
+
+  // TESTING THE WATCHER.
+}
